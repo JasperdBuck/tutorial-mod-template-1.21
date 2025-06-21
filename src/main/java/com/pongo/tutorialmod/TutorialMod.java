7bd84bf092e74301d8ac4b8f021db5a8a6afb146
jaspe <jasperdebuck3@gmail.com>
2025-06-21 20:11:09 +0200

    added 2 blocks

--- src/main/java/com/pongo/tutorialmod/TutorialMod.java
+++ src/main/java/com/pongo/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package com.pongo.tutorialmod;
 
+import com.pongo.tutorialmod.block.ModBlocks;
 import com.pongo.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -14,5 +15,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
